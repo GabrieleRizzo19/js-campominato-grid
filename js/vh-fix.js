e35c35d13066ad25c9a3b9af562412640f7d99a4
GabrieleRizzo19 <gabryrizz02@gmail.com>
2023-04-26 16:56:22 +0200

    add footer and layout fix

---
+++ js/vh-fix.js
@@ -0,0 +1,7 @@
+window.addEventListener('resize', function() {
+    document.documentElement.style.setProperty('--fix-vh', `${window.innerHeight/100}px`);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.documentElement.style.setProperty('--fix-vh', `${window.innerHeight/100}px`);
+});
